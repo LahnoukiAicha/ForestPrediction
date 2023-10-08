d9b25a07329186168a5f4e877f455835fcd837b6
Aicha Lahnouki <Aichalahnouki@gmail.com>
2023-10-08 19:29:39 +0100

    aicha lahnouki

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask,request,  render_template
+import pickle
+import numpy as np
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+print("Before loading the model")
+model = pickle.load(open('model.pkl', 'rb'))
+print("After loading the model")
+
+
+@app.route('/')
+def hello_world():
+    return render_template("forest_fire.html")
+
+
+@app.route('/predict',methods=['POST','GET'])
+def predict():
+    int_features=[int(x) for x in request.form.values()]
+    final=[np.array(int_features)]
+    print(int_features)
+    print(final)
+    prediction=model.predict_proba(final)
+    output='{0:.{1}f}'.format(prediction[0][1], 2)
+
+    if output>str(0.5):
+        return render_template('forest_fire.html',pred='Your Forest is in Danger.\nProbability of fire occuring is {}'.format(output))
+    else:
+        return render_template('forest_fire.html',pred='Your Forest is safe.\n Probability of fire occuring is {}'.format(output))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
